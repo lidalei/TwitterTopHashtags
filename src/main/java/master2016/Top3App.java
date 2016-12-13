e707270e27ca65d708272cced63aab92b14281d6
Dalei Li <lidalei151781@gmail.com>
2016-12-13 12:33:38 +0100

    Implement cluster mode.

--- src/main/java/master2016/Top3App.java
+++ src/main/java/master2016/Top3App.java
@@ -1,7 +1,11 @@
 package master2016;
 
 import org.apache.storm.Config;
-import org.apache.storm.LocalCluster;
+//import org.apache.storm.LocalCluster;
+import org.apache.storm.StormSubmitter;
+import org.apache.storm.generated.AlreadyAliveException;
+import org.apache.storm.generated.AuthorizationException;
+import org.apache.storm.generated.InvalidTopologyException;
 import org.apache.storm.topology.TopologyBuilder;
 import org.apache.storm.tuple.Fields;
 import org.apache.storm.utils.Utils;
@@ -15,7 +19,6 @@ public class Top3App {
 
     public static void main(String[] args) {
 
-        /*
         if(args.length < 4) {
             System.out.println("Not enough parameters. There should be four parameters.");
             return;
@@ -49,17 +52,17 @@ public class Top3App {
         String outputFolder = args[3];
 
         System.out.println("Output folder: " + outputFolder);
-        */
+
 
 
         // for debug use only. // TODO, change to previous piece of code
-        HashMap<String, String> langTokenDict = new HashMap<>(4);
-        langTokenDict.put("en", "house");
-        langTokenDict.put("es", "ordenador");
-
-        String kafkaBrokerURL = "localhost:9092";
-        String topologyName = "Topology";
-        String outputFolder = "/Users/Sophie/";
+//        HashMap<String, String> langTokenDict = new HashMap<>(4);
+//        langTokenDict.put("en", "house");
+//        langTokenDict.put("es", "ordenador");
+//
+//        String kafkaBrokerURL = "138.4.110.141:41604";
+//        String topologyName = "Topology";
+//        String outputFolder = "~/";
 
 
         final String groupID = "YesWeCan";
@@ -67,20 +70,37 @@ public class Top3App {
         // build topology
         TopologyBuilder topologyBuilder = new TopologyBuilder();
 
-        topologyBuilder.setSpout("KafkaSpout", new KafkaSpout(langTokenDict, kafkaBrokerURL, groupID));
-        topologyBuilder.setBolt("Top3Bolt", new TwitterTopKBolt(langTokenDict, outputFolder, 3))
+        // the number of executors for each spout / bolt, and each executor has one task by default
+        int parallelismHint = langTokenDict.size();
+
+        topologyBuilder.setSpout("KafkaSpout", new KafkaSpout(langTokenDict, kafkaBrokerURL, groupID), parallelismHint);
+        topologyBuilder.setBolt("Top3Bolt", new TwitterTopKBolt(langTokenDict, outputFolder, 3), parallelismHint)
                 .fieldsGrouping("KafkaSpout", KafkaSpout.TWITTER_STREAM_NAME, new Fields(KafkaSpout.LANGUAGE_NAME));
 
 
-        // TODO, test in the cluster
+        Config clusterConfig = new Config();
+        // there are three workers in the cluster
+        clusterConfig.setNumWorkers(3);
+
         // local model
-        LocalCluster locClu = new LocalCluster();
-        locClu.submitTopology(topologyName, new Config(), topologyBuilder.createTopology());
+//        LocalCluster locClu = new LocalCluster();
+//        locClu.submitTopology(topologyName, new Config(), topologyBuilder.createTopology());
+//        locClu.killTopology(topologyName);
+//        locClu.shutdown();
+
+        // submit topology to the cluster
+        StormSubmitter submitter = new StormSubmitter();
+        try {
+            submitter.submitTopology(topologyName, clusterConfig, topologyBuilder.createTopology());
+        } catch (AlreadyAliveException e) {
+            e.printStackTrace();
+        } catch (InvalidTopologyException e) {
+            e.printStackTrace();
+        } catch (AuthorizationException e) {
+            e.printStackTrace();
+        }
 
         Utils.sleep(100000);
-
-        locClu.killTopology(topologyName);
-        locClu.shutdown();
 
     }
 
